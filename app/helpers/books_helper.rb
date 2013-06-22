e65c6379db836ada01b727b5d66614e445485dfa
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-22 22:39:29 +0900

    Add embedding_tag and embedding_url helper methods

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,14 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Bibid::App.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def embedding_tag(book)
+    tag :iframe, :src => embedding_url(book)
+  end
+
+  def embedding_url(book)
+    request_uri = URI(request.url)
+    request_uri.path = "/components/bibi/bib/i/"
+    request_uri.query = "book=#{book}.epub"
+    request_uri.to_s
+  end
 end
