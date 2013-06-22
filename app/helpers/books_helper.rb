5f9540d21533b3be0837c64745c9e5693382f8e5
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-22 23:31:07 +0900

    Fix representation

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,8 +1,9 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Bibid::App.helpers do
+  # @todo Make width and height customizable
   def embedding_tag(book)
-    tag :iframe, :src => embedding_url(book)
+    tag :iframe, :src => embedding_url(book), :width => '100%', :height => '100%'
   end
 
   def embedding_url(book)
