fb1375a814b9821518efff427613e53e76c7193a
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-23 01:32:08 +0900

    Raise exception on booting if session secret not set

--- config/apps.sample.rb
+++ config/apps.sample.rb
@@ -27,7 +27,8 @@
 #
 Padrino.configure_apps do
   # enable :sessions
-  set :session_secret, '01b730daf02343fb470231c6a1f4db8e8c0b755e48c5b2fb2659c65065f69011'
+  raise "Configure session_secret such like '01b730daf02343fb470231c6a1f4db8e8c0b755e48c5b2fb2659c65065f69011'"
+  set :session_secret, 'Determine session secret and write here'
   set :protection, true
   set :protect_from_csrf, true
 end
