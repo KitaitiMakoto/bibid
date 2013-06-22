159847079d00f6fa87bbd48eeada67abcdc8c38e
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-23 01:30:48 +0900

    Make salt of EpubUploader attribute

--- app/uploaders/epub_uploader.rb
+++ app/uploaders/epub_uploader.rb
@@ -1,13 +1,21 @@
 class EpubUploader < CarrierWave::Uploader::Base
   EXTENSION = '.epub'
 
+  class << self
+    attr_accessor :salt
+  end
+
   storage :file
 
   def filename
-    Digest::SHA1.hexdigest(SALT + Time.now.to_s) + EXTENSION
+    Digest::SHA1.hexdigest(salt + Time.now.to_s) + EXTENSION
   end
 
   def store_dir
     'components/bibi/bib/bookshelf'
+  end
+
+  def salt
+    self.class.salt
   end
 end
