8d69c59be44a00fa627d7cb7634bfa0a04d69908
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-22 10:54:29 +0900

    Generate BooksController

---
+++ app/controllers/books.rb
@@ -0,0 +1,34 @@
+Bibid::App.controllers :books do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :upload do
+
+  end
+
+  post :upload do
+
+  end
+
+  get :show do
+
+  end
+
+end
