3cd94dc94f333eb87dbe31f28a3d366ff90e9d39
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-07-16 05:05:03 +0900

    Add constraints to users table

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -2,9 +2,11 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :name
-      t.string :display_name
+      t.string :display_name, :null => false
       t.timestamps
     end
+
+    add_index :users, :name
   end
 
   def self.down
