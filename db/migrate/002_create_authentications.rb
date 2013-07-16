cfab1159da56478f37348b56fc146dce6978b7fb
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-07-17 02:16:11 +0900

    Define authentications table

--- db/migrate/002_create_authentications.rb
+++ db/migrate/002_create_authentications.rb
@@ -1,11 +1,15 @@
 class CreateAuthentications < ActiveRecord::Migration
   def self.up
     create_table :authentications do |t|
-      t.integer :user_id
-      t.string :provider
-      t.string :uid
+      t.integer :user_id, :null => false
+      t.string :provider, :null => false
+      t.string :uid, :null => false
       t.timestamps
     end
+
+    add_foreign_key :authentications, :users, :dependent => :delete
+    add_index :authentications, [:user_id, :provider], :unique => true
+    add_index :authentications, [:provider, :uid], :unique => true
   end
 
   def self.down
