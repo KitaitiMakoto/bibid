84a9b19ff14e3da4b042e2565bc0c1ebb9fd0012
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-07-17 19:52:03 +0900

    Move auth process to books sessions controller

--- app/app.rb
+++ app/app.rb
@@ -75,30 +75,6 @@ module Bibid
       render 'books/index'
     end
 
-    get '/auth/:provider/callback' do
-      auth = request.env['omniauth.auth']
-      unless auth.provider == params[:provider]
-        raise
-      end
-      authentication = Authentication.find_by_provider_and_uid(auth.provider, auth.uid)
-      if authentication
-        session.clear
-        session[:user_id] = authentication.user.id
-        redirect url(:users, :show, :name => current_user.name)
-      elsif current_user
-        current_user.authentications.create provider: auth.provider, uid: auth.uid
-        redirect url(:users, :show, :name => current_user.name)
-      else
-        session[:auth] = {
-          :provider     => auth.provider,
-          :uid          => auth.uid,
-          :name         => Authentication.name_from(auth),
-          :display_name => Authentication.display_name_from(auth)
-        }
-        redirect url(:users, :new)
-      end
-    end
-
     get '/auth/failure' do
       "Authentication with #{h params['strategy']} failed: #{h params['message']}"
     end
--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -23,6 +23,30 @@ Bibid::App.controllers :sessions do
     render 'sessions/new'
   end
 
+  get :create, :map => '/auth/:provider/callback' do
+    auth = request.env['omniauth.auth']
+    unless auth.provider == params[:provider]
+      raise
+    end
+    authentication = Authentication.find_by_provider_and_uid(auth.provider, auth.uid)
+    if authentication
+      session.clear
+      session[:user_id] = authentication.user.id
+      redirect url(:users, :show, :name => current_user.name)
+    elsif current_user
+      current_user.authentications.create provider: auth.provider, uid: auth.uid
+      redirect url(:users, :show, :name => current_user.name)
+    else
+      session[:auth] = {
+        :provider     => auth.provider,
+        :uid          => auth.uid,
+        :name         => Authentication.name_from(auth),
+        :display_name => Authentication.display_name_from(auth)
+      }
+      redirect url(:users, :new)
+    end
+  end
+
   delete :destroy, :map => '/session' do
     session.clear
     redirect '/'
