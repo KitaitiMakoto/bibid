78bb4d788262257d06c1ca5aeaa1ce326fe58170
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-07-19 01:51:05 +0900

    Add NOT NULL constraint to books.epub

--- db/migrate/003_create_books.rb
+++ db/migrate/003_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.integer :user_id, :null => false
       t.string :title
-      t.string :epub
+      t.string :epub, :null => false
       t.timestamps
     end
 
--- models/book.rb
+++ models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   belongs_to :user
   mount_uploader :epub, EpubUploader
+
+  validates :epub, :presence => true
 end
