01a450ead50725978bf223cb189d9232d8b5661d
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-07-18 22:34:29 +0900

    Use subdirectory for stored EPUB fies

--- lib/epub_uploader.rb
+++ lib/epub_uploader.rb
@@ -12,7 +12,7 @@ class EpubUploader < CarrierWave::Uploader::Base
   end
 
   def store_dir
-    'components/bibi/bib/bookshelf'
+    File.join('components/bibi/bib/bookshelf', model.user.name)
   end
 
   def salt
