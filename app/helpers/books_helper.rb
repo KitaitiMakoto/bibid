54177ca5bd7b9eaaf6a9e5b33edb65d44378b037
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-07-18 21:58:06 +0900

    Append end tag of iframe

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -3,7 +3,7 @@
 Bibid::App.helpers do
   # @todo Make width and height customizable
   def embedding_tag(book)
-    tag :iframe, :src => embedding_url(book), :width => '100%', :height => '100%'
+    content_tag(:iframe, :src => embedding_url(book), :width => '100%', :height => '100%') {''}
   end
 
   def embedding_url(book)
