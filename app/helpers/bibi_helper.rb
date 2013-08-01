75a97663f5824a135fb806f5b33766d6520f3d63
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-08-01 11:09:05 +0900

    Add textareas to show embedding tags for feeds

---
+++ app/helpers/bibi_helper.rb
@@ -0,0 +1,17 @@
+Bibid::App.helpers do
+  def bibi_url
+    '/components/bibi/bib'
+  end
+
+  def absolute_bibi_url
+    uri(bibi_path, true, false)
+  end
+
+  def bookshelf_url(book=nil)
+    parts = [absolute_bibi_url, 'bookshelf']
+    if book
+      parts << book.user.name << File.basename(book.epub.current_path)
+    end
+    parts.join('/')
+  end
+end
