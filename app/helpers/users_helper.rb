aa09ae27b7f8fdd2b24591d2e6bfcc439163fd99
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-08-04 09:51:39 +0900

    SELECT current user only when signed in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@
 
 Bibid::App.helpers do
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def opds_from_user(user)
