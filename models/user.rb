ea0aef650a673b6299e255d6d7371e5d54706c46
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-08-04 10:34:53 +0900

    [BUG FIX]current_file_size is zero when user has no book

--- models/user.rb
+++ models/user.rb
@@ -13,7 +13,9 @@ class User < ActiveRecord::Base
   }
 
   def current_file_size
-    store_dir_path = File.join(Padrino.root, 'public', books.first.epub.store_dir)
+    sample_book = books.first
+    return 0 unless sample_book
+    store_dir_path = File.join(Padrino.root, 'public', sample_book.epub.store_dir)
     Dir["#{store_dir_path}/*"].reduce(0) {|total, epub| total + File.size(epub)}
   end
 end
