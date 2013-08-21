e103d4f9fc1bb9992194a7bb3a9c281b5915a305
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-08-22 01:59:29 +0900

    Remove unneccessary comments

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,4 @@
-# Helper methods defined here can be accessed in any controller or view in the application
-
 Bibid::App.helpers do
-  # @todo Make width and height customizable
   def embedding_tag(book, options={})
     options = {:'data-bibi' => 'embed', :'data-bibi-style' => 'width: 100%; height: 100%;'}.merge(options)
     link_to(book.title, embedding_url(book), options) +
