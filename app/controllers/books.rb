35e5c1ffb89fa8cfecc9cd7c7a0227c2fa764a7e
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-01-23 00:45:30 +0900

    Nest books route under users

--- app/controllers/books.rb
+++ app/controllers/books.rb
@@ -24,35 +24,4 @@ Bibid::App.controllers :books do
 
     render 'books/index'
   end
-
-  post :create, :map => '/books', :require_sign_in => true do
-    @book = Book.new(params[:book])
-    @book.title = EPUB::Parser.parse(@book.epub.current_path).title rescue File.basename(@book.epub.filename, '.*')
-    if current_user.books << @book
-      redirect url(:books, :show, :id => @book.id), :success => I18n.t('notice.books.create')
-    else
-      render 'books/new'
-    end
-  end
-
-  get :show, :map => '/books/:id' do
-    if @book = Book.find_by_id(params[:id])
-      render 'books/show'
-    else
-      halt 404
-    end
-  end
-
-  delete :destroy, :map => '/books/:id', :require_sign_in => true do
-    if @book = Book.find_by_id(params[:id])
-      if current_user == @book.user
-        @book.destroy
-        redirect url(:users, :show, :name => current_user.name), :success => I18n.t('notice.books.destroy')
-      else
-        halt 403
-      end
-    else
-      halt 404
-    end
-  end
 end
---
+++ app/controllers/users_books.rb
@@ -0,0 +1,27 @@
+Bibid::App.controllers :users_books, :parent => :users do
+  get :show, :map => 'books/:id' do
+    logger.info params.inspect
+    user = User.find_by_name(params[:user_id])
+    return halt 404 unless user
+    @book = user.books.find_by_id(params[:id])
+    return halt 404 unless @book
+    render 'books/show'
+  end
+
+  post :create, :map => 'books', :require_sign_in => true do
+    @book = current_user.books.build(params[:book])
+    @book.title = EPUB::Parser.parse(@book.epub.current_path).title rescue File.basename(@book.epub.filename, '.*')
+    if current_user.books << @book
+      redirect url(:users_books, :show, :user_id => @book.user.name, :id => @book.id), :success => I18n.t('notice.books.create')
+    else
+      render 'books/new'
+    end
+  end
+
+  delete :destroy, :map => 'books/:id', :require_sign_in => true do
+    @book = current_user.books.find_by_id(params[:id])
+    return halt 404 unless @book
+    @book.destroy
+    redirect url(:users, :show, :name => current_user.name), :success => I18n.t('notice.books.destroy')
+  end
+end
