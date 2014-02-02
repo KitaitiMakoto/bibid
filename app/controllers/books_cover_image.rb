8cead27a059b8289d7b142f3fc8f1cef160dfaa1
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-02-03 08:58:43 +0900

    Change route: cover_image -> cover-image

--- app/controllers/books_cover_image.rb
+++ app/controllers/books_cover_image.rb
@@ -1,5 +1,5 @@
 Bibid::App.controllers :books_cover_image, :parent => [:users, :books] do
-  get :show, :map => 'cover_image' do
+  get :show, :map => 'cover-image' do
     user = User.find_by_name(params[:user_id])
     return halt 404 unless user
     book = user.books.find_by_id(params[:book_id])
