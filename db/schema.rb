61ff761044f6db2707ce466c6ec198006dcfce40
KITAITI Makoto <KitaitiMakoto@gmail.com>
2018-03-08 14:03:56 +0900

    Run migration
        % bundle exec be rake ar:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string   "epub",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "file_size",  null: false
   end
 
   add_index "books", ["epub"], name: "index_books_on_epub", unique: true, using: :btree
