035c5a178e2c43c26ae2ef88c25e8b60bb155fa2
KITAITI Makoto <KitaitiMakoto@gmail.com>
2018-03-08 14:03:41 +0900

    Make books.file_size not null

--- db/migrate/004_add_fields_to_books.rb
+++ db/migrate/004_add_fields_to_books.rb
@@ -1,8 +1,17 @@
+require_relative "../../models/user"
+require_relative "../../models/book"
+
 class AddFieldsToBooks < ActiveRecord::Migration
   def self.up
     change_table :books do |t|
       t.integer :file_size
     end
+
+    Book.find_each do |book|
+      book.update file_size: book.epub.file.size
+    end
+
+    change_column :books, :file_size, :integer, null: false
   end
 
   def self.down
