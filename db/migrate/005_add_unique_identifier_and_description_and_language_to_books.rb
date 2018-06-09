b709301f6030c87504960a031a8152dc9d83a556
KITAITI Makoto <KitaitiMakoto@gmail.com>
2018-06-10 04:26:07 +0900

    Add books.unique_identifier, description, language fields
        % padrino generate migration AddUniqueIdentifierAndDescriptionAndLanguageToBooks unique_identifier:string description
    :text language:string

---
+++ db/migrate/005_add_unique_identifier_and_description_and_language_to_books.rb
@@ -0,0 +1,17 @@
+class AddUniqueIdentifierAndDescriptionAndLanguageToBooks < ActiveRecord::Migration
+  def self.up
+    change_table :books do |t|
+      t.string :unique_identifier
+    t.text :description
+    t.string :language
+    end
+  end
+
+  def self.down
+    change_table :books do |t|
+      t.remove :unique_identifier
+    t.remove :description
+    t.remove :language
+    end
+  end
+end
