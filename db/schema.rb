d817da390def9f4e899dc1d0bdc2fff0b734c5c0
KITAITI Makoto <KitaitiMakoto@gmail.com>
2018-06-10 06:46:48 +0900

    Execute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authentications", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.string   "provider",   null: false
-    t.string   "uid",        null: false
+  create_table "authentications", id: :serial, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid", unique: true
+    t.index ["user_id", "provider"], name: "index_authentications_on_user_id_and_provider", unique: true
   end
 
-  add_index "authentications", ["provider", "uid"], name: "index_authentications_on_provider_and_uid", unique: true, using: :btree
-  add_index "authentications", ["user_id", "provider"], name: "index_authentications_on_user_id_and_provider", unique: true, using: :btree
-
-  create_table "books", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.string   "title"
-    t.string   "epub",       null: false
+  create_table "books", id: :serial, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.string "epub", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "file_size",  null: false
+    t.integer "file_size", null: false
+    t.string "unique_identifier"
+    t.text "description"
+    t.string "language"
+    t.index ["epub"], name: "index_books_on_epub", unique: true
   end
 
-  add_index "books", ["epub"], name: "index_books_on_epub", unique: true, using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "display_name", null: false
-    t.string   "lower_name",   null: false
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "display_name", null: false
+    t.string "lower_name", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["lower_name"], name: "index_users_on_lower_name"
+    t.index ["name"], name: "index_users_on_name"
   end
-
-  add_index "users", ["lower_name"], name: "index_users_on_lower_name", using: :btree
-  add_index "users", ["name"], name: "index_users_on_name", using: :btree
 
   add_foreign_key "authentications", "users"
   add_foreign_key "books", "users"
